a4781200c1fde9a9d4f1078b01e8f490941e1b94
nourzaraa <nour.zarra@esprit.tn>
2026-04-02 21:54:39 +0100

    commiiiittt

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/login-form"
 
 export default function LoginPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-muted p-6 md:p-10">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-4xl">
         <LoginForm />
       </div>
